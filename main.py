2e54f660912be86fed926ddd7507f78600834da1
euuce <147289911+euuce@users.noreply.github.com>
2023-12-29 02:10:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ else:
     print(f"{FILE_PATH} already exists")
 
 # Clean old files
-paths_to_delete = ['boot.log', 'list.txt','sub.txt', 'swith', 'web', 'bot', 'tunnel.yml', 'tunnel.json']
+paths_to_delete = ['boot.log', 'list.txt','sub.txt', 'npm', 'web', 'bot', 'tunnel.yml', 'tunnel.json']
 for file in paths_to_delete:
     file_path = os.path.join(FILE_PATH, file)
     try:
@@ -115,20 +115,20 @@ def download_files_and_run():
             print(f"Download {file_info['file_name']} failed: {e}")
 
     # Authorize and run
-    files_to_authorize = ['./swith', './web', './bot']
+    files_to_authorize = ['./npm', './web', './bot']
     authorize_files(files_to_authorize)
 
     # Run ne-zha
     NEZHA_TLS = ''
     if NEZHA_SERVER and NEZHA_PORT and NEZHA_KEY:
         NEZHA_TLS = '--tls' if NEZHA_PORT == '443' else ''
-        command = f"nohup {FILE_PATH}/swith -s {NEZHA_SERVER}:{NEZHA_PORT} -p {NEZHA_KEY} {NEZHA_TLS} >/dev/null 2>&1 &"
+        command = f"nohup {FILE_PATH}/npm -s {NEZHA_SERVER}:{NEZHA_PORT} -p {NEZHA_KEY} {NEZHA_TLS} >/dev/null 2>&1 &"
         try:
             subprocess.run(command, shell=True, check=True)
-            print('swith is running')
+            print('npm is running')
             subprocess.run('sleep 1', shell=True)  # Wait for 1 second
         except subprocess.CalledProcessError as e:
-            print(f'swith running error: {e}')
+            print(f'npm running error: {e}')
     else:
         print('NEZHA variable is empty, skip running')
 
@@ -183,13 +183,13 @@ def get_cloud_flare_args():
 def get_files_for_architecture(architecture):
     if architecture == 'arm':
         return [
-            {'file_name': 'swith', 'file_url': 'https://github.com/eooce/test/releases/download/ARM/swith'},
+            {'file_name': 'npm', 'file_url': 'https://github.com/eooce/test/releases/download/ARM/swith'},
             {'file_name': 'web', 'file_url': 'https://github.com/eooce/test/releases/download/ARM/web'},
             {'file_name': 'bot', 'file_url': 'https://github.com/eooce/test/releases/download/ARM/server'},
         ]
     elif architecture == 'amd':
         return [
-            {'file_name': 'swith', 'file_url': 'https://github.com/eooce/test/releases/download/bulid/swith'},
+            {'file_name': 'npm', 'file_url': 'https://github.com/eooce/test/releases/download/amd64/npm'},
             {'file_name': 'web', 'file_url': 'https://github.com/eooce/test/releases/download/123/web'},
             {'file_name': 'bot', 'file_url': 'https://github.com/eooce/test/raw/main/server'},
         ]
